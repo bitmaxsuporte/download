8b2240ca51a2c087fc8a8a91a6a555107ed04d13
Jean de Santana <51293417+xjeansantana@users.noreply.github.com>
2024-05-09 12:09:29 -0300

    Atualizacao

---
+++ src/js/info.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var texto = xhr.responseText;
+            exibirPopup(texto);
+        }
+    };
+    xhr.open("GET", "./info.txt", true);
+    xhr.send();
+};
+
+function exibirPopup(texto) {
+    alert(texto);
+}
